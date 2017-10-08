648d0da93723f64b88e77cd830d5e1eb5d756dd4
Cris <cristian.manaila@gmail.com>
2017-10-08 23:31:39 +0300

    added some basic code

--- trader/main-trader.py
+++ trader/main-trader.py
@@ -1,10 +1,36 @@
+# Libs
 import krakenex
+import time
 
+# Variables
+parameters = {"pair" : "XXRPZEUR"}
+currentMovingAverage = 0
+prices = []
+lastTrade = 0
+
+# Krakenex init 1
 k = krakenex.API()
 
+#Key loader from only 2 comps
+#k.load_key('')
 
-k.load_key('')
+# Latency check
+start = k.query_public('Time')
+end = k.query_public('Time')
+latency = end['result']['unixtime']-start['result']['unixtime']
+
+print('latency: ' + str(latency))
 
 
+timeout = time.time() + 60*60*24
+while True:
+    #print(type(k.query_public('Ticker',{"pair" : "XXRPZEUR"})))
+    data = k.query_public('Ticker',{"pair" : "XXRPZEUR"})
+    lastTrade = float(data['result']['XXRPZEUR']['c'][0])
+    prices.append(lastTrade)
+    print("Kraken - XRP/EUR - last transaction %r | moving average %r" % (lastTrade, currentMovingAverage))
+    time.sleep(14)
+    if time.time() > timeout:
+        break
 
-print(k.query_private('Balance'))
+#lastTrade = float(data['result']['XXRPZEUR']['c'][0])
